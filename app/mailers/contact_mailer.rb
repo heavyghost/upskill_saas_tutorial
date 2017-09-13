6701423612fe43ce6ca989ac7196b8c41efac4be
Jack Markovs <j.markovs@gmail.com>
2017-09-13 03:34:02 +0000

    Changed contact email address

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-   default to: 'jack.markovs@uwa.edu.au' 
+   default to: 'j.markovs@gmail.com' 
    
    def contact_email(name, email, body)
        @name = name
