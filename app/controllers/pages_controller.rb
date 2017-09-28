668fd4c1002bd60bb09ef33161ff7d53fb15881d
Jack Markovs <j.markovs@gmail.com>
2017-09-28 03:01:57 +0000

    Added plan vars params to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,10 @@
 class PagesController < ApplicationController
+  # Get request for '/'  which is out home page
   def home
+    @basic_plan = Plan.find(1)
+    @pro_plan = Plan.find(2)
   end
-  
+  # Get request for '/about-us'  which is out about page
   def about
   end
 end
