42731e17bc39c478c75a70da38b219c4a0bc25f2
Jack Markovs <j.markovs@gmail.com>
2018-09-03 08:27:23 +0000

    Added profiles controller, new view & link on homepage

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,7 @@
+class ProfilesController < ApplicationController
+   
+   # GET to /users/:user_id/profile/new
+   def new
+        # Render black profile details form
+   end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
     root to: 'pages#home'
     devise_for :users, controllers: {registrations: 'users/registrations'}
     resources :users do
-        resource :profiles
+        resource :profile
+    end
         
     get 'about', to: 'pages#about'
     resources :contacts, only: :create
